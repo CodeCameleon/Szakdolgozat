6bbd8a678edd861a17535df5f93120638bd07573
h267659 <h267659@stud.u-szeged.hu>
2024-10-21 16:30:50 +0200

    Karakterkészlet enum és helper megvalósítva.

---
+++ MathCrypt/Enums/ECharset.cs
@@ -0,0 +1,42 @@
+﻿namespace MathCrypt.Enums;
+
+/// <summary>
+/// A karakterkészleteket tartalamzó enum.
+/// </summary>
+internal enum ECharset
+{
+    /// <summary>
+    /// Szóköz
+    /// </summary>
+    Space = 0,
+
+    /// <summary>
+    /// Számok
+    /// </summary>
+    Numbers = 1,
+
+    /// <summary>
+    /// Matematikai szimbólumok
+    /// </summary>
+    MathSymbols = 2,
+
+    /// <summary>
+    /// Írásjelek
+    /// </summary>
+    Punctuations = 3,
+
+    /// <summary>
+    /// Speciális karakterek
+    /// </summary>
+    Special = 4,
+
+    /// <summary>
+    /// Angol nyelv karakterei
+    /// </summary>
+    EN = 10,
+
+    /// <summary>
+    /// Magyar nyelv karakterei
+    /// </summary>
+    HU = 11
+}
---
+++ MathCrypt/Helpers/CharsetHelper.cs
@@ -0,0 +1,69 @@
+﻿using MathCrypt.Enums;
+
+namespace MathCrypt.Helpers;
+
+/// <summary>
+/// A karakterkészleteket segítő statikus osztály.
+/// </summary>
+internal static class CharsetHelper
+{
+    /// <summary>
+    /// A karakterkészleteket tartalmazó szótár.
+    /// </summary>
+    private static readonly Dictionary<ECharset, string> _charsetData = new()
+    {
+        { ECharset.Space, " " },
+        { ECharset.Numbers, "0123456789" },
+        { ECharset.MathSymbols, "+-*/=<>(){}[]^%" },
+        { ECharset.Punctuations, "!\"#$&',.:;?@\\_`|~" },
+        { ECharset.Special, "\r\n\t\b\f" },
+        { ECharset.EN, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+        { ECharset.HU, "áéíóöőúüűÁÉÍÓÖŐÚÜŰ" }
+    };
+
+    /// <summary>
+    /// Az alapértelmezett karakterkészletet tartalmazó halmaz.
+    /// </summary>
+    private static readonly HashSet<ECharset> _default =
+    [
+        ECharset.Space,
+        ECharset.Numbers,
+        ECharset.EN,
+    ];
+
+    /// <summary>
+    /// Lekéri a karakterkészlethez tartozó karaktereket.
+    /// </summary>
+    /// <param name="charsets">A keresett karakterkészletek.</param>
+    /// <returns>A karakterkészlet karakter tömb formájában.</returns>
+    public static char[] GetCharacters(params ECharset[] charsets)
+    {
+        return ECharsetsToChars(charsets);
+    }
+
+    /// <summary>
+    /// Lekéri az alapértelmezett karakterkészletet.
+    /// </summary>
+    /// <returns>Az alapértelmezett karakterkészlet karakter tömb formájában.</returns>
+    public static char[] GetDefaultCharsets()
+    {
+        return ECharsetsToChars(_default.ToArray());
+    }
+
+    /// <summary>
+    /// Visszaadja a karakterkészlethez tartozó karaktereket.
+    /// </summary>
+    /// <param name="charsets">A keresett karakterkészletek.</param>
+    /// <returns>A karakterkészlet karakter tömb formájában.</returns>
+    private static char[] ECharsetsToChars(ECharset[] charsets)
+    {
+        List<char> chars = [];
+
+        foreach (ECharset charset in charsets)
+        {
+            chars.AddRange(_charsetData[charset]);
+        }
+
+        return chars.ToArray();
+    }
+}
