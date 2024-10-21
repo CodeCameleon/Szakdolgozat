f362144aa25c28f24c5993cf8e01266ba527ea1e
h267659 <h267659@stud.u-szeged.hu>
2024-10-21 21:33:49 +0200

    A pont és a különleges lista modellek megvalósítva.

---
+++ MathCrypt/Models/Point.cs
@@ -0,0 +1,37 @@
+﻿namespace MathCrypt.Models;
+
+/// <summary>
+/// Egy pontot ábrázoló osztály.
+/// </summary>
+internal class Point
+{
+    /// <summary>
+    /// A pont sorszáma.
+    /// </summary>
+    public int Row { get; set; }
+
+    /// <summary>
+    /// A pont oszlopszáma.
+    /// </summary>
+    public int Column { get; set; }
+
+    /// <summary>
+    /// Az osztály konstruktora.
+    /// </summary>
+    /// <param name="row">A pont sorszáma.</param>
+    /// <param name="column">A pont oszlopszáma.</param>
+    public Point(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    /// <summary>
+    /// Vissza ad egy karakterláncot, amely az aktuális objektumot reprezentálja.
+    /// </summary>
+    /// <returns>Egy karakterlánc, amely az aktuális objektumot reprezentálja.</returns>
+    public override string ToString()
+    {
+        return $"({Row}, {Column})";
+    }
+}
---
+++ MathCrypt/Models/ShuffleList.cs
@@ -0,0 +1,71 @@
+﻿namespace MathCrypt.Models;
+
+/// <summary>
+/// Egy különleges listát megvalósító osztály.
+/// </summary>
+/// <typeparam name="T">Az elem tipusa, amit a lista tárolni fog.</typeparam>
+internal class ShuffleList<T>
+    where T : class
+{
+    /// <summary>
+    /// A listát tároló adattag.
+    /// </summary>
+    private readonly List<T> _list;
+
+    /// <summary>
+    /// Az aktuális elem indexét tároló adattag.
+    /// </summary>
+    private int _index;
+
+    /// <summary>
+    /// Az osztály konstruktora.
+    /// </summary>
+    public ShuffleList()
+    {
+        _list = [];
+        _index = 0;
+    }
+
+    /// <summary>
+    /// Hozzáad egy új elemet a lista végére.
+    /// </summary>
+    /// <param name="item">A hozzáadni kivánt elem.</param>
+    public void Add(T item)
+    {
+        _list.Add(item);
+    }
+
+    /// <summary>
+    /// Visszaadja a listából a következő elemet.
+    /// Ha eléri a végét, akkor megkeveri és újraindul az elejéről.
+    /// </summary>
+    /// <returns>Az aktuális elem a listából.</returns>
+    public T Next()
+    {
+        if (_list.Count != 0)
+        {
+            T current = _list[_index];
+            _index++;
+
+            if (_index == _list.Count)
+            {
+                Shuffle();
+                _index = 0;
+            }
+
+            return current;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Megkeveri a lista elemeit.
+    /// </summary>
+    public void Shuffle()
+    {
+        // TODO Megvalósítani
+    }
+}
