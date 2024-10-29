b4daf6ec69ea27199f860553ea65b0f07f9483ef
h267659 <h267659@stud.u-szeged.hu>
2024-10-29 11:02:41 +0100

    MathCrypt tesztosztály megvalósítva.

---
+++ AlgorithmTest/Models/IAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace AlgorithmTest.Models;
+
+/// <summary>
+/// Egy általános titkosító algoritmust ábrázoló interfész.
+/// </summary>
+internal interface IAlgorithm
+{
+    /// <summary>
+    /// Titkosítja a megadott szöveget.
+    /// </summary>
+    /// <param name="plaintext">A titkosítandó szöveg.</param>
+    /// <returns>A titkosított szöveg és a futási idő.</returns>
+    (string CipherText, Stopwatch TimeToRun) Encrypt(string plaintext);
+
+    /// <summary>
+    /// Visszafejti a megadott szöveget.
+    /// </summary>
+    /// <param name="ciphertext">A visszafejtendő szöveg.</param>
+    /// <returns>A visszafejtett szöveg és a futási idő.</returns>
+    (string PlainText, Stopwatch TimeToRun) Decrypt(string ciphertext);
+}
---
+++ AlgorithmTest/Models/MathCryptAlgorithm.cs
@@ -0,0 +1,59 @@
+﻿using MathCrypt.Services;
+using System.Diagnostics;
+
+namespace AlgorithmTest.Models;
+
+/// <summary>
+/// A MathCrypt titkosító algoritmust megvalósító osztály.
+/// </summary>
+internal class MathCryptAlgorithm
+    : IAlgorithm
+{
+    /// <summary>
+    /// A titkosító algoritmust tároló adattag.
+    /// </summary>
+    private readonly CryptionService _service;
+
+    /// <summary>
+    /// A futási idő mérésére szolgáló osztályt tároló adattag.
+    /// </summary>
+    private readonly Stopwatch _stopwatch;
+
+    /// <summary>
+    /// Az osztály paraméteres konstruktora.
+    /// </summary>
+    /// <param name="key">A kulcs, amelyet a titkosító algoritmus használ.</param>
+    public MathCryptAlgorithm(char[][] key)
+    {
+        _service = new CryptionService(key);
+        _stopwatch = new Stopwatch();
+    }
+
+    /// <summary>
+    /// Titkosítja a megadott szöveget.
+    /// </summary>
+    /// <param name="plaintext">A titkosítandó szöveg.</param>
+    /// <returns>A titkosított szöveg és a futási idő.</returns>
+    public (string CipherText, Stopwatch TimeToRun) Encrypt(string plaintext)
+    {
+        _stopwatch.Restart();
+        string ciphertext = _service.Encrypt(plaintext);
+        _stopwatch.Stop();
+
+        return (ciphertext, _stopwatch);
+    }
+
+    /// <summary>
+    /// Visszafejti a megadott szöveget.
+    /// </summary>
+    /// <param name="ciphertext">A visszafejtendő szöveg.</param>
+    /// <returns>A visszafejtett szöveg és a futási idő.</returns>
+    public (string PlainText, Stopwatch TimeToRun) Decrypt(string ciphertext)
+    {
+        _stopwatch.Restart();
+        string plaintext = _service.Decrypt(ciphertext);
+        _stopwatch.Stop();
+
+        return (plaintext, _stopwatch);
+    }
+}
