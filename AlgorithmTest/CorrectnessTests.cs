467978cd95ad6223e0345ba10ed80fe96eb042cc
h267659 <h267659@stud.u-szeged.hu>
2024-10-29 12:28:41 +0100

    Pontosságot vizsgáló tesztosztály megvalósítva.

---
+++ AlgorithmTest/CorrectnessTests.cs
@@ -0,0 +1,66 @@
+﻿using AlgorithmTest.Helpers;
+using AlgorithmTest.Models;
+using MathCrypt.Enums;
+using MathCrypt.Services;
+using System.Diagnostics;
+
+namespace AlgorithmTest;
+
+/// <summary>
+/// A titkosító algoritmusok pontosságát vizsgáló tesztesetek.
+/// </summary>
+[TestFixture]
+internal class CorrectnessTests
+{
+    /// <summary>
+    /// Az értékek, amikel az osztály a teszteket végzni.
+    /// </summary>
+    private static readonly object[] TestCases =
+    [
+        new object[] { "Alma" },
+        new object[] { "Bannán" },
+        new object[] { "Narancs" },
+        new object[] { "Ez már kicsit nehezebb!" },
+        new object[] { "4-5-1 és még 4 talán 2 is?" }
+    ];
+
+    /// <summary>
+    /// A MathCrypt titkosító algoritmus tároló adattag.
+    /// </summary>
+    private MathCryptAlgorithm _mathCrpyt;
+
+    /// <summary>
+    /// A teszteket előkészítő függvény.
+    /// </summary>
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _mathCrpyt = new(KeyGenService.Instance
+            .GenerateKey(
+                strength: 2,
+                ECharset.Space,
+                ECharset.Numbers,
+                ECharset.MathSymbols,
+                ECharset.Punctuations,
+                ECharset.EN,
+                ECharset.HU
+            )
+        );
+    }
+
+    /// <summary>
+    /// A MathCrypt titkosító algoritmus pontosságát vizsgáló teszt.
+    /// </summary>
+    /// <param name="input">A szöveg, amivel a teszt dolgozik.</param>
+    [Test, TestCaseSource(nameof(TestCases))]
+    public void MathCryptCorrectness(string input)
+    {
+        (string cipherText, Stopwatch encryptTime) = _mathCrpyt.Encrypt(input);
+        TestContext.WriteLine(StringHelper.TimeToEncrypt(encryptTime));
+        
+        (string plainText, Stopwatch decryptTime) = _mathCrpyt.Decrypt(cipherText);
+        TestContext.WriteLine(StringHelper.TimeToDecrypt(decryptTime));
+
+        Assert.That(plainText, Is.EqualTo(input), StringHelper.Error.Correctness(_mathCrpyt));
+    }
+}
---
+++ AlgorithmTest/Helpers/StringHelper.cs
@@ -0,0 +1,46 @@
+﻿using AlgorithmTest.Models;
+using System.Diagnostics;
+
+namespace AlgorithmTest.Helpers;
+
+/// <summary>
+/// A szövegműveleteket segítő statikus osztály.
+/// </summary>
+internal static class StringHelper
+{
+    /// <summary>
+    /// A hibaüzeneteket tartalmazó belső osztály.
+    /// </summary>
+    public static class Error
+    {
+        /// <summary>
+        /// A pontossági teszt sikertelensége esetén dobandó hibaüzenet.
+        /// </summary>
+        /// <param name="algorithm">Az algoritmus, amely megbukott a teszten.</param>
+        /// <returns>A hibaüzenet.</returns>
+        public static string Correctness(IAlgorithm algorithm)
+        {
+            return $"A {nameof(algorithm)} megbukott a pontosségi teszten.";
+        }
+    }
+
+    /// <summary>
+    /// A titkosításhoz szükséges időt kiíró üzenet.
+    /// </summary>
+    /// <param name="stopwatch">Az időt tartalmazó objektum.</param>
+    /// <returns>Az üzenet.</returns>
+    public static string TimeToEncrypt(Stopwatch stopwatch)
+    {
+        return $"A titkosítás lefutott {stopwatch.Elapsed} ms alatt.";
+    }
+
+    /// <summary>
+    /// A visszafejtéshez szükséges időt kiíró üzenet.
+    /// </summary>
+    /// <param name="stopwatch">Az időt tartalmazó objektum.</param>
+    /// <returns>Az üzenet.</returns>
+    public static string TimeToDecrypt(Stopwatch stopwatch)
+    {
+        return $"A visszafejtés lefutott {stopwatch.Elapsed} ms alatt.";
+    }
+}
