50b0257b047a0068db851faff6b639a5bda8f1c5
h267659 <h267659@stud.u-szeged.hu>
2025-05-07 14:17:30 +0200

    Egy ConsoleApp hozzádava.

---
+++ Thesis.ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿using Thesis.MathCrypt.Implementations;
+
+MathCryptKeyGenerator keyGenerator = new();
+MathCrypt mathCrypt = new(keyGenerator.GenerateKey(strength: 2));
+
+Console.WriteLine(mathCrypt.ToString());
